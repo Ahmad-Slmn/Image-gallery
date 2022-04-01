356d194f4ebad58ebceb43ee73655ac1f0e6846e
AhmedSlmn <adverts890@gmail.com>
2022-04-01 15:54:32 +0300

    Upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+let button = document.getElementsByClassName("btn");
+
+let img = document.getElementById("banner");
+
+button[0].onclick = function () {
+
+    img.src = "images/pic3.png";
+
+    animate();
+
+    this.classList.add("active")
+
+
+}
+
+button[1].onclick = function () {
+
+    img.src = "images/pic2.png";
+
+    animate();
+
+    this.classList.add("active")
+}
+
+button[2].onclick = function () {
+
+    img.src = "images/pic1.jpg";
+
+    animate();
+
+    this.classList.add("active")
+}
+
+function animate() {
+
+    img.classList.add("zoom");
+
+    setTimeout(function () {
+
+        img.classList.remove("zoom")
+
+    }, 500);
+
+    for (b of button) {
+
+        b.classList.remove("active")
+    }
+}
